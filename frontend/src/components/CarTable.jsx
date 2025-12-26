af69e3b7866afdd08cd25a691e8a53d12d8115a0
Michael <michaelagedie71@gmail.com>
2025-12-26 17:45:26 -0600

    add title and car table

--- frontend/src/components/CarTable.jsx
+++ frontend/src/components/CarTable.jsx
@@ -3,17 +3,21 @@ import ProfitBadge from "./ProfitBadge";
 const mockCars = [
   {
     id: 1,
+    titleStatus: "SALVAGE",
     year: 2016,
     make: "Toyota",
     model: "Camry",
+    mileage: 145000,
     price: 6800,
     profit: 2700
   },
   {
     id: 2,
+    titleStatus: "CLEAN",
     year: 2014,
     make: "Honda",
     model: "Civic",
+    mileage: 145000,
     price: 7200,
     profit: -500
   }
@@ -27,16 +31,27 @@ export default function CarTable() {
           <th>Car</th>
           <th>Buy Price</th>
           <th>Expected Profit</th>
+          <th>Title</th>
         </tr>
       </thead>
       <tbody>
-        {mockCars.map(car => (
-          <tr key={car.id}>
-            <td>{car.year} {car.make} {car.model}</td>
-            <td>${car.price}</td>
-            <td><ProfitBadge profit={car.profit} /></td>
-          </tr>
-        ))}
+        {mockCars.map(car => {
+          // Decision logic lives in JavaScript
+          const titleColor = car.titleStatus === "CLEAN" ? "green" : "red";
+
+          return (
+            <tr key={car.id}>
+              <td>{car.year} {car.make} {car.model}</td>
+              <td>${car.price}</td>
+              <td><ProfitBadge profit={car.profit} /></td>
+              <td>
+                <span style={{ color: titleColor }}>
+                  {car.titleStatus}
+                </span>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
